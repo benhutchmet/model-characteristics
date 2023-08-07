e46c6a106764a10511dfaba02e18feb4fc4f9fd2
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-08-07 12:30:59 +0100

    debugging none found error

--- dictionaries.py
+++ dictionaries.py
@@ -4,6 +4,8 @@ canari_dir = "/gws/nopw/j04/canari/users/benhutch/"
 
 models = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "HadGEM3-GC31-MM", "EC-Earth3", "MPI-ESM1-2-LR", "FGOALS-f3-L", "MIROC6", "IPSL-CM6A-LR", "CESM1-1-CAM5-CMIP5", "NorCPM1" ]
 
+historical_models = [ "BCC-CSM2-MR", "MPI-ESM1-2-HR", "CanESM5", "CMCC-CM2-SR5", "HadGEM3-GC31-MM", "EC-Earth3", "MPI-ESM1-2-LR", "FGOALS-f3-L", "MIROC6", "IPSL-CM6A-LR", "NorCPM1" ]
+
 variables = [ "psl", "tas", "tos", "rsds", "sfcWind" ]
 
 # data on JASMIN path
