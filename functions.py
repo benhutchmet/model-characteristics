a61503d047ba74f28f219ffa10ae23db9a86ba5e
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-08-07 14:40:08 +0100

    files list added as a column

--- functions.py
+++ functions.py
@@ -347,7 +347,8 @@ def fill_dataframe(models, base_path, variables, columns, experiment, table_id):
         "no_members": lambda model, base_path, variable: get_variable(model, base_path, experiment=experiment, table_id=table_id, variable=variable)[1],
         "members_list": lambda model, base_path, variable: get_variable(model, base_path, experiment=experiment, table_id=table_id, variable=variable)[2],
         "variable": lambda model, base_path, variable: get_variable(model, base_path, experiment=experiment, table_id=table_id, variable=variable)[0],
-        "model": lambda model, base_path, variable: model
+        "model": lambda model, base_path, variable: model,
+        "files_list": lambda model, base_path, variable: get_files(model, base_path, experiment=experiment, table_id=table_id, variable=variable)
     }
 
     # iterate over the models and variables
